f0654b17e3ec97f9206f328bae6e08e306f5ef8c
Mohan Lar <mohan.lar@soaren.io>
2023-10-20 12:45:21 -0700

    - all of the code added

---
+++ code/src/code/learnmohan/Main.java
@@ -0,0 +1,12 @@
+package code.learnmohan;
+
+public class Main {
+    public static void main(String[] names) {
+        Array a = new Array(3);
+        a.insert(3);
+        a.insert(4);
+        a.insert(3);
+        a.print();
+    }
+}
+
